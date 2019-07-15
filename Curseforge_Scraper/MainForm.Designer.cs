044a7beb9a36c85e6afecc1242b44e6efb29c4e9
Quintus Rijpstra <32038610+qjnr@users.noreply.github.com>
2019-07-15 18:38:02 +0200

    Add project files.

---
+++ Curseforge_Scraper/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Curseforge_Scraper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textbox_SearchQuery = new System.Windows.Forms.TextBox();
+            this.labelInfo_Search = new System.Windows.Forms.Label();
+            this.button_Search = new System.Windows.Forms.Button();
+            this.listView_Results = new System.Windows.Forms.ListView();
+            this.imageList_Results = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // textbox_SearchQuery
+            // 
+            this.textbox_SearchQuery.Location = new System.Drawing.Point(15, 25);
+            this.textbox_SearchQuery.Name = "textbox_SearchQuery";
+            this.textbox_SearchQuery.Size = new System.Drawing.Size(317, 20);
+            this.textbox_SearchQuery.TabIndex = 0;
+            // 
+            // labelInfo_Search
+            // 
+            this.labelInfo_Search.AutoSize = true;
+            this.labelInfo_Search.Location = new System.Drawing.Point(12, 9);
+            this.labelInfo_Search.Name = "labelInfo_Search";
+            this.labelInfo_Search.Size = new System.Drawing.Size(41, 13);
+            this.labelInfo_Search.TabIndex = 1;
+            this.labelInfo_Search.Text = "Search";
+            // 
+            // button_Search
+            // 
+            this.button_Search.Location = new System.Drawing.Point(338, 23);
+            this.button_Search.Name = "button_Search";
+            this.button_Search.Size = new System.Drawing.Size(89, 23);
+            this.button_Search.TabIndex = 2;
+            this.button_Search.Text = "Search";
+            this.button_Search.UseVisualStyleBackColor = true;
+            this.button_Search.Click += new System.EventHandler(this.Button_Search_Click);
+            // 
+            // listView_Results
+            // 
+            this.listView_Results.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView_Results.LargeImageList = this.imageList_Results;
+            this.listView_Results.Location = new System.Drawing.Point(12, 51);
+            this.listView_Results.MultiSelect = false;
+            this.listView_Results.Name = "listView_Results";
+            this.listView_Results.Size = new System.Drawing.Size(415, 425);
+            this.listView_Results.TabIndex = 3;
+            this.listView_Results.UseCompatibleStateImageBehavior = false;
+            this.listView_Results.View = System.Windows.Forms.View.Tile;
+            this.listView_Results.DoubleClick += new System.EventHandler(this.ListView_Results_DoubleClick);
+            // 
+            // imageList_Results
+            // 
+            this.imageList_Results.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList_Results.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageList_Results.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.button_Search;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 488);
+            this.Controls.Add(this.listView_Results);
+            this.Controls.Add(this.button_Search);
+            this.Controls.Add(this.labelInfo_Search);
+            this.Controls.Add(this.textbox_SearchQuery);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Cureforge Scraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox_SearchQuery;
+        private System.Windows.Forms.Label labelInfo_Search;
+        private System.Windows.Forms.Button button_Search;
+        private System.Windows.Forms.ListView listView_Results;
+        private System.Windows.Forms.ImageList imageList_Results;
+    }
+}
+
---
+++ Curseforge_Scraper/MainForm.cs
@@ -0,0 +1,90 @@
+﻿using HtmlAgilityPack;
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Curseforge_Scraper
+{
+    public partial class MainForm : Form
+    {
+        HtmlWeb htmlWeb = new HtmlWeb();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Search_Click(object sender, EventArgs e)
+        {
+            string searchQuery = textbox_SearchQuery.Text;
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                // unpopulate listview
+                imageList_Results.Images.Clear();
+                listView_Results.Items.Clear();
+
+                // get data from web
+                var page = await htmlWeb.LoadFromWebAsync("https://www.curseforge.com/minecraft/mc-mods/search?search=" + Uri.EscapeDataString(searchQuery));
+                var textNodes = page.DocumentNode.SelectNodes("/html/body/div[1]/main/div[1]/div[3]/ul/div/div/div/div[2]/div[1]/a[1]/h3");
+                var imageNodes = page.DocumentNode.SelectNodes("/html/body/div[1]/main/div[1]/div[3]/ul/div/div/div/div[1]/div[1]/div/a/img");
+                var linkNodes = page.DocumentNode.SelectNodes("/html/body/div[1]/main/div[1]/div[3]/ul/div/div/div/div[2]/div[1]/a[1]");
+
+                // populate listview
+                for (int i = 0; i < imageNodes.Count; i++)
+                {
+                    var imageNode = imageNodes[i];
+                    var url = imageNode.Attributes["src"].Value;
+                    // download associated image
+                    using (var wc = new WebClient())
+                    {
+                        using (var ms = new MemoryStream(wc.DownloadData(url)))
+                        {
+                            using (var img = Image.FromStream(ms))
+                            {
+                                // add items
+                                imageList_Results.Images.Add(i.ToString(), img);
+                                var textNode = textNodes[i];
+                                var listViewItem = new ListViewItem(WebUtility.HtmlDecode(textNode.InnerText), i.ToString());
+                                var linkNode = linkNodes[i];
+                                listViewItem.ToolTipText = linkNode.Attributes["href"].Value;
+                                listView_Results.Items.Add(listViewItem);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void ListView_Results_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView_Results.SelectedItems.Count == 1)
+            {
+                var listViewItem = listView_Results.SelectedItems[0];
+                var url = listViewItem.ToolTipText;
+
+                // get the download page
+                var page = await htmlWeb.LoadFromWebAsync("https://www.curseforge.com" + url + "/download");
+                var downloadLink = page.DocumentNode.SelectSingleNode("/html/body/div[1]/main/div[1]/div/div[2]/div/div[1]/p[2]/a");
+                var downloadUrl = downloadLink.Attributes["href"].Value;
+
+                // ask for file location
+                var sfd = new SaveFileDialog();
+                sfd.Filter = "JAR file|*.jar";
+                sfd.Title = "Download mod";
+                sfd.FileName = new Regex("[^a-zA-Z0-9-]").Replace(listViewItem.Text, "");
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    using (var wc = new WebClient())
+                    {
+                        wc.DownloadFileAsync(new Uri("https://www.curseforge.com" + downloadUrl), sfd.FileName);
+                    }
+                }
+            }
+        }
+    }
+}
