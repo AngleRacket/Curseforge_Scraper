e5f838b5531b7c281a3ba3d94e97ac0e0bfc8dd5
Quintus Rijpstra <32038610+qjnr@users.noreply.github.com>
2019-07-16 15:36:59 +0200

    Add missing features #1 & #2
    - Specify Minecraft version
    - Automatically download dependencies

--- Curseforge_Scraper/MainForm.Designer.cs
+++ Curseforge_Scraper/MainForm.Designer.cs
@@ -35,6 +35,14 @@
             this.button_Search = new System.Windows.Forms.Button();
             this.listView_Results = new System.Windows.Forms.ListView();
             this.imageList_Results = new System.Windows.Forms.ImageList(this.components);
+            this.radioButton_McVersion_1_12 = new System.Windows.Forms.RadioButton();
+            this.labelInfo_McVersion = new System.Windows.Forms.Label();
+            this.checkBox_DownloadDependencies = new System.Windows.Forms.CheckBox();
+            this.radioButton_McVersion_1_11 = new System.Windows.Forms.RadioButton();
+            this.radioButton_McVersion_1_10 = new System.Windows.Forms.RadioButton();
+            this.radioButton_McVersion_1_9 = new System.Windows.Forms.RadioButton();
+            this.radioButton_McVersion_1_8 = new System.Windows.Forms.RadioButton();
+            this.radioButton_McVersion_1_7 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // textbox_SearchQuery
@@ -82,12 +90,108 @@
             this.imageList_Results.ImageSize = new System.Drawing.Size(64, 64);
             this.imageList_Results.TransparentColor = System.Drawing.Color.Transparent;
             // 
+            // radioButton_McVersion_1_12
+            // 
+            this.radioButton_McVersion_1_12.AutoSize = true;
+            this.radioButton_McVersion_1_12.Checked = true;
+            this.radioButton_McVersion_1_12.Location = new System.Drawing.Point(437, 51);
+            this.radioButton_McVersion_1_12.Name = "radioButton_McVersion_1_12";
+            this.radioButton_McVersion_1_12.Size = new System.Drawing.Size(46, 17);
+            this.radioButton_McVersion_1_12.TabIndex = 4;
+            this.radioButton_McVersion_1_12.TabStop = true;
+            this.radioButton_McVersion_1_12.Text = "1.12";
+            this.radioButton_McVersion_1_12.UseVisualStyleBackColor = true;
+            // 
+            // labelInfo_McVersion
+            // 
+            this.labelInfo_McVersion.AutoSize = true;
+            this.labelInfo_McVersion.Location = new System.Drawing.Point(433, 28);
+            this.labelInfo_McVersion.Name = "labelInfo_McVersion";
+            this.labelInfo_McVersion.Size = new System.Drawing.Size(89, 13);
+            this.labelInfo_McVersion.TabIndex = 5;
+            this.labelInfo_McVersion.Text = "Minecraft Version";
+            // 
+            // checkBox_DownloadDependencies
+            // 
+            this.checkBox_DownloadDependencies.AutoSize = true;
+            this.checkBox_DownloadDependencies.Checked = true;
+            this.checkBox_DownloadDependencies.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_DownloadDependencies.Location = new System.Drawing.Point(436, 325);
+            this.checkBox_DownloadDependencies.Name = "checkBox_DownloadDependencies";
+            this.checkBox_DownloadDependencies.Size = new System.Drawing.Size(141, 30);
+            this.checkBox_DownloadDependencies.TabIndex = 6;
+            this.checkBox_DownloadDependencies.Text = "Automatically download\r\nadditional dependencies";
+            this.checkBox_DownloadDependencies.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_McVersion_1_11
+            // 
+            this.radioButton_McVersion_1_11.AutoSize = true;
+            this.radioButton_McVersion_1_11.Location = new System.Drawing.Point(436, 74);
+            this.radioButton_McVersion_1_11.Name = "radioButton_McVersion_1_11";
+            this.radioButton_McVersion_1_11.Size = new System.Drawing.Size(46, 17);
+            this.radioButton_McVersion_1_11.TabIndex = 7;
+            this.radioButton_McVersion_1_11.TabStop = true;
+            this.radioButton_McVersion_1_11.Text = "1.11";
+            this.radioButton_McVersion_1_11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_McVersion_1_10
+            // 
+            this.radioButton_McVersion_1_10.AutoSize = true;
+            this.radioButton_McVersion_1_10.Location = new System.Drawing.Point(437, 97);
+            this.radioButton_McVersion_1_10.Name = "radioButton_McVersion_1_10";
+            this.radioButton_McVersion_1_10.Size = new System.Drawing.Size(46, 17);
+            this.radioButton_McVersion_1_10.TabIndex = 8;
+            this.radioButton_McVersion_1_10.TabStop = true;
+            this.radioButton_McVersion_1_10.Text = "1.10";
+            this.radioButton_McVersion_1_10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_McVersion_1_9
+            // 
+            this.radioButton_McVersion_1_9.AutoSize = true;
+            this.radioButton_McVersion_1_9.Location = new System.Drawing.Point(437, 120);
+            this.radioButton_McVersion_1_9.Name = "radioButton_McVersion_1_9";
+            this.radioButton_McVersion_1_9.Size = new System.Drawing.Size(40, 17);
+            this.radioButton_McVersion_1_9.TabIndex = 9;
+            this.radioButton_McVersion_1_9.TabStop = true;
+            this.radioButton_McVersion_1_9.Text = "1.9";
+            this.radioButton_McVersion_1_9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_McVersion_1_8
+            // 
+            this.radioButton_McVersion_1_8.AutoSize = true;
+            this.radioButton_McVersion_1_8.Location = new System.Drawing.Point(437, 143);
+            this.radioButton_McVersion_1_8.Name = "radioButton_McVersion_1_8";
+            this.radioButton_McVersion_1_8.Size = new System.Drawing.Size(40, 17);
+            this.radioButton_McVersion_1_8.TabIndex = 10;
+            this.radioButton_McVersion_1_8.TabStop = true;
+            this.radioButton_McVersion_1_8.Text = "1.8";
+            this.radioButton_McVersion_1_8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_McVersion_1_7
+            // 
+            this.radioButton_McVersion_1_7.AutoSize = true;
+            this.radioButton_McVersion_1_7.Location = new System.Drawing.Point(437, 166);
+            this.radioButton_McVersion_1_7.Name = "radioButton_McVersion_1_7";
+            this.radioButton_McVersion_1_7.Size = new System.Drawing.Size(40, 17);
+            this.radioButton_McVersion_1_7.TabIndex = 11;
+            this.radioButton_McVersion_1_7.TabStop = true;
+            this.radioButton_McVersion_1_7.Text = "1.7";
+            this.radioButton_McVersion_1_7.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AcceptButton = this.button_Search;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 488);
+            this.ClientSize = new System.Drawing.Size(578, 488);
+            this.Controls.Add(this.radioButton_McVersion_1_7);
+            this.Controls.Add(this.radioButton_McVersion_1_8);
+            this.Controls.Add(this.radioButton_McVersion_1_9);
+            this.Controls.Add(this.radioButton_McVersion_1_10);
+            this.Controls.Add(this.radioButton_McVersion_1_11);
+            this.Controls.Add(this.checkBox_DownloadDependencies);
+            this.Controls.Add(this.labelInfo_McVersion);
+            this.Controls.Add(this.radioButton_McVersion_1_12);
             this.Controls.Add(this.listView_Results);
             this.Controls.Add(this.button_Search);
             this.Controls.Add(this.labelInfo_Search);
@@ -107,6 +211,14 @@
         private System.Windows.Forms.Button button_Search;
         private System.Windows.Forms.ListView listView_Results;
         private System.Windows.Forms.ImageList imageList_Results;
+        private System.Windows.Forms.RadioButton radioButton_McVersion_1_12;
+        private System.Windows.Forms.Label labelInfo_McVersion;
+        private System.Windows.Forms.CheckBox checkBox_DownloadDependencies;
+        private System.Windows.Forms.RadioButton radioButton_McVersion_1_11;
+        private System.Windows.Forms.RadioButton radioButton_McVersion_1_10;
+        private System.Windows.Forms.RadioButton radioButton_McVersion_1_9;
+        private System.Windows.Forms.RadioButton radioButton_McVersion_1_8;
+        private System.Windows.Forms.RadioButton radioButton_McVersion_1_7;
     }
 }
 
--- Curseforge_Scraper/MainForm.cs
+++ Curseforge_Scraper/MainForm.cs
@@ -30,60 +30,110 @@ namespace Curseforge_Scraper
 
                 // get data from web
                 var page = await htmlWeb.LoadFromWebAsync("https://www.curseforge.com/minecraft/mc-mods/search?search=" + Uri.EscapeDataString(searchQuery));
-                var textNodes = page.DocumentNode.SelectNodes("/html/body/div[1]/main/div[1]/div[3]/ul/div/div/div/div[2]/div[1]/a[1]/h3");
                 var imageNodes = page.DocumentNode.SelectNodes("/html/body/div[1]/main/div[1]/div[3]/ul/div/div/div/div[1]/div[1]/div/a/img");
                 var linkNodes = page.DocumentNode.SelectNodes("/html/body/div[1]/main/div[1]/div[3]/ul/div/div/div/div[2]/div[1]/a[1]");
 
-                // populate listview
-                for (int i = 0; i < imageNodes.Count; i++)
+                // check if there are any search results
+                if (imageNodes != null)
                 {
-                    var imageNode = imageNodes[i];
-                    var url = imageNode.Attributes["src"].Value;
-                    // download associated image
-                    using (var wc = new WebClient())
+
+                    // populate listview
+                    for (int i = 0; i < imageNodes.Count; i++)
                     {
-                        using (var ms = new MemoryStream(wc.DownloadData(url)))
+                        var imageNode = imageNodes[i];
+                        var url = imageNode.Attributes["src"].Value;
+                        // download associated image
+                        using (var wc = new WebClient())
                         {
-                            using (var img = Image.FromStream(ms))
+                            using (var ms = new MemoryStream(wc.DownloadData(url)))
                             {
-                                // add items
-                                imageList_Results.Images.Add(i.ToString(), img);
-                                var textNode = textNodes[i];
-                                var listViewItem = new ListViewItem(WebUtility.HtmlDecode(textNode.InnerText), i.ToString());
-                                var linkNode = linkNodes[i];
-                                listViewItem.ToolTipText = linkNode.Attributes["href"].Value;
-                                listView_Results.Items.Add(listViewItem);
+                                using (var img = Image.FromStream(ms))
+                                {
+                                    // add items
+                                    imageList_Results.Images.Add(i.ToString(), img);
+                                    var linkNode = linkNodes[i];
+                                    var listViewItem = new ListViewItem(WebUtility.HtmlDecode(linkNode.InnerText), i.ToString());
+                                    listViewItem.ToolTipText = linkNode.Attributes["href"].Value;
+                                    listView_Results.Items.Add(listViewItem);
+                                }
                             }
                         }
                     }
+
+                } else
+                {
+                    MessageBox.Show("No search results for query: \"" + searchQuery + "\"", "Curseforge Scraper", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }
 
-        private async void ListView_Results_DoubleClick(object sender, EventArgs e)
+        private void ListView_Results_DoubleClick(object sender, EventArgs e)
         {
             if (listView_Results.SelectedItems.Count == 1)
             {
                 var listViewItem = listView_Results.SelectedItems[0];
                 var url = listViewItem.ToolTipText;
 
-                // get the download page
-                var page = await htmlWeb.LoadFromWebAsync("https://www.curseforge.com" + url + "/download");
-                var downloadLink = page.DocumentNode.SelectSingleNode("/html/body/div[1]/main/div[1]/div/div[2]/div/div[1]/p[2]/a");
+                DownloadMod(url, listViewItem.Text);
+            }
+        }
+
+        private async void DownloadMod(string url, string modName)
+        {
+            // get minecraft version
+            string humanReadableMcVersion =
+                radioButton_McVersion_1_12.Checked ? "1.12" :
+                radioButton_McVersion_1_11.Checked ? "1.11" :
+                radioButton_McVersion_1_10.Checked ? "1.10" :
+                radioButton_McVersion_1_9.Checked ? "1.9" :
+                radioButton_McVersion_1_8.Checked ? "1.8" :
+                radioButton_McVersion_1_7.Checked ? "1.7" : null; // will never be null
+            string mcVersionString =
+                radioButton_McVersion_1_12.Checked ? "1738749986%3a628" :
+                radioButton_McVersion_1_11.Checked ? "1738749986%3a599" :
+                radioButton_McVersion_1_10.Checked ? "1738749986%3a572" :
+                radioButton_McVersion_1_9.Checked ? "1738749986%3a552" :
+                radioButton_McVersion_1_8.Checked ? "1738749986%3a4" :
+                radioButton_McVersion_1_7.Checked ? "1738749986%3a5" : null; // will never be null
+
+            // get the download page
+            var page = await htmlWeb.LoadFromWebAsync("https://www.curseforge.com" + url + "/files/all?filter-game-version=" + mcVersionString);
+            var downloadLink = page.DocumentNode.SelectSingleNode("/html/body/div[1]/main/div[1]/div[2]/section/div/div/div/section/div[2]/div/table/tbody/tr[1]/td[2]/a");
+            if (downloadLink != null)
+            {
                 var downloadUrl = downloadLink.Attributes["href"].Value;
 
                 // ask for file location
                 var sfd = new SaveFileDialog();
                 sfd.Filter = "JAR file|*.jar";
                 sfd.Title = "Download mod";
-                sfd.FileName = new Regex("[^a-zA-Z0-9-]").Replace(listViewItem.Text, "");
+                sfd.FileName = new Regex("[^a-zA-Z0-9-]").Replace(modName, "") + "_" + humanReadableMcVersion;
                 if (sfd.ShowDialog() == DialogResult.OK)
                 {
                     using (var wc = new WebClient())
                     {
                         wc.DownloadFileAsync(new Uri("https://www.curseforge.com" + downloadUrl), sfd.FileName);
                     }
+
+                    if (checkBox_DownloadDependencies.Checked)
+                    {
+                        // download additional dependencies
+                        var dependencyPage = await htmlWeb.LoadFromWebAsync("https://www.curseforge.com" + url + "/relations/dependencies");
+                        var dependencies = dependencyPage.DocumentNode.SelectNodes("/html/body/div[1]/main/div[1]/div[2]/div/div/div[3]/ul/li/div[2]/div[1]/a[1]");
+
+                        if (dependencies != null)
+                        {
+                            foreach (var dependencyLink in dependencies)
+                            {
+                                DownloadMod(dependencyLink.Attributes["href"].Value, dependencyLink.InnerText);
+                            }
+                        }
+                    }
                 }
+            }
+            else
+            {
+                MessageBox.Show("The specified Minecraft version is not available for this mod.", "Curseforge Scraper", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
